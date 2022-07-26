d9201ad568f9849142ab7468e18a2ef6b49e56b4
gnaa <gamaliagina@Gamalias-MacBook-Pro.local>
2022-07-26 15:53:27 +0700

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,16 +1,11 @@
+ 
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
  
 const init = async () => {
-
   const server = Hapi.server({
-    port: 5000,
+    port: 8001,
     host: 'localhost',
-    routes:{
-      cors:{
-        origin: ['*'],
-      },
-    },
   });
  
   server.route(routes);
